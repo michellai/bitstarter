1125969ae93754c6a27f343611a9668484a20236
Michelle Lai <michellai@gmail.com>
2013-07-10 06:42:51 +0000

    Express buffer populated by index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var index_buffer = fs.readFileSync('index.html')
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index_buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
